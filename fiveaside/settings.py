4d91fe0a7b511ae301143921bdd9ebac5fddfd60
cramshaw <chris.ramshaw@gmail.com>
2015-08-03 20:33:31 +0100

    static

--- fiveaside/settings.py
+++ fiveaside/settings.py
@@ -119,8 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'fives/static'),
 )
 
 LOGIN_URL = '/login/'

d6565058313e7d4d1051e64aa554d5f2c549473f
cramshaw <chris.ramshaw@gmail.com>
2015-08-04 09:16:14 +0100

    fix urls?

--- fiveaside/urls.py
+++ fiveaside/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^fives/', include('fives.urls')),
+    url(r'^/', include('fives.urls')),
 ]
